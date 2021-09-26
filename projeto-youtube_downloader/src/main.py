db3af8d548faedad1da70f1c9bcdfc5cc64b6619
João Rodrigues <j_neto_rodrigues@hotmail.com>
2021-09-26 11:24:15 -0300

    projeto simples usando pytube

---
+++ projeto-youtube_downloader/src/main.py
@@ -0,0 +1,106 @@
+from pytube import YouTube
+import pprint
+from pytube import Search
+from pytube import Playlist
+from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import *
+
+
+# link =  input("digite seu link:")
+# link = "https://www.youtube.com/watch?v=tEcs_Hf6r_E"
+# print(link)
+
+def isYoutubePlaylist():
+    if link.get().__contains__("list="):
+        print("playlist detectado")
+        return True
+    else:
+        print("link unico detectado")
+        return False
+
+
+def isYoutubeLink():
+    if link.get().__contains__("https://www.youtube.com/"):
+        return True
+    else:
+        return False
+
+
+main = Tk()
+main.title('VIDEO DOWNLOADER')
+
+
+def youtube_link():
+    blank.delete(0, END)
+    print(link.get())
+    if(not isYoutubeLink()):
+        blank.insert(0,"link não válido")
+    else:
+
+        if(isYoutubePlaylist()):
+            print("trabalhando com playlist")
+            blank.insert(0, "playlist preparada para download")
+            yt = YouTube(link.get())
+            print(yt.streams.filter(progressive=True, file_extension='mp4'))
+            title.insert(0, "PLAYLIST: " + yt.title)
+
+        else:
+            print("trabalhando o link único")
+            blank.insert(0, "video preparado para download")
+            yt = YouTube(link.get())
+            print(yt.streams.filter(progressive=True, file_extension='mp4'))
+            title.insert(0,yt.title)
+
+
+def download():
+    blank.delete(0, END)
+    blank.insert(0, "fazendo o download... aguarde")
+    print(monthchoosen.current())
+
+
+# def sub():
+# blank.delete(0, END)
+# Ans = int(num1.get()) - int(num2.get())
+# blank.insert(0, Ans)
+
+
+main.geometry('900x300')
+Label(main, text="Youtube Link: ").grid(row=0)
+Label(main, text="Video Title: ").grid(row=1)
+Label(main, text="Output:").grid(row=2)
+
+link = Entry(main)
+title = Entry(main)
+blank = Entry(main)
+
+
+monthchoosen = ttk.Combobox(main, width=27)
+
+# Adding combobox drop down list
+monthchoosen['values'] = (' MP4 360p',
+                          ' MP4 720p',
+                          ' March',
+                          ' April',
+                          ' May',
+                          ' June',
+                          ' July',
+                          ' August',
+                          ' September',
+                          ' October',
+                          ' November',
+                          ' December')
+
+monthchoosen.grid(column=1, row=5)
+monthchoosen.current()
+
+link.grid(row=0, column=1)
+title.grid(row=1, column=1)
+blank.grid(row=2, column=1)
+
+Button(main, text='Quit', command=main.destroy).grid(row=10, column=1, sticky=W)
+Button(main, text='Procurar', command=youtube_link).grid(row=0, column=3, sticky=W, )
+Button(main, text='Download', command=download).grid(row=3, column=1, sticky=W)
+
+
+mainloop()
